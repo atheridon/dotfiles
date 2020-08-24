7b5eeeecf622a74e2634a416e5e2f75629246f62
atheridon <atheridon@gmail.com>
2020-08-24 21:06:11 +0200

    bash.bashrc

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -4,10 +4,11 @@ dwmblocks &
 xwallpaper --stretch ~/owncloud/Pictures/Wallpaper/wallpaper.png &
 dunst &
 picom &
+~/owncloud/Linux/scripts/battcheck start &
+xss-lock ~/owncloud/Linux/scripts/lock &
+xautolock -time 10 -locker ~/owncloud/Linux/scripts/sleeplock &
 if [ `pidof owncloud ` ]; then
 	killall owncloud
 	sleep 5
 fi
 owncloud &
-xss-lock ~/owncloud/Linux/scripts/lock &
-xautolock -time 10 -locker ~/owncloud/Linux/scripts/sleeplock &
