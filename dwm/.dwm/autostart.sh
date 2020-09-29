240b1e1ba7793f5a6d4d1c4f5c64bfbddc6ca032
atheridon <atheridon@gmail.com>
2020-09-29 15:47:13 +0200

    adjusted

--- dwm/.dwm/autostart.sh
+++ dwm/.dwm/autostart.sh
@@ -5,11 +5,10 @@ xwallpaper --stretch ~/owncloud/Pictures/Wallpaper/lichking.jpg &
 dunst &
 picom &
 ~/owncloud/Linux/scripts/battcheck start &
-xss-lock ~/owncloud/Linux/scripts/lock &
-xautolock -time 10 -locker ~/owncloud/Linux/scripts/sleeplock &
 if [ `pidof owncloud ` ]; then
 	killall owncloud
 	sleep 5
 fi
 owncloud &
+blueman-applet &
 nm-applet &
